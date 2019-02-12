ef2bd0f44b305ce85c2bb39f89e1e493c6428304
Admin <Admin@Admin-PC>
2019-02-12 15:26:47 +0100

    add bean

---
+++ src/main/java/com/express/aliExpress_offre/rest/ProduitRest.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.express.aliExpress_offre.rest;
+
+/**
+ *
+ * @author pc asus
+ */
+public class ProduitRest {
+    
+}
